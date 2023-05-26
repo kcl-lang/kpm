9b90f0433ffeb51f77be2d3d26b7266247c1dd98
zongz <zongzhe1024@163.com>
2023-05-26 18:24:33 +0800

    fix: fix default repo.

--- pkg/settings/settings.go
+++ pkg/settings/settings.go
@@ -23,7 +23,7 @@ type KpmConf struct {
 }
 
 const DEFAULT_REGISTRY = "ghcr.io"
-const DEFAULT_REPO = "KusionStack"
+const DEFAULT_REPO = "kusionstack"
 
 // This is a singleton that loads kpm settings from 'kpm.json'
 // and is only initialized on the first call by 'Init()' or 'GetSettings()'
