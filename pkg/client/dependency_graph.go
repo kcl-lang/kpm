a2c36b7c66f3efeb8288865f8dba0d844388df08
Vanshika <vanshikav928@gmail.com>
2024-02-13 13:25:38 +0530

    pretest
    Signed-off-by: Vanshika <vanshikav928@gmail.com>

---
+++ pkg/client/dependency_graph.go
@@ -0,0 +1,46 @@
+package client
+
+// Construct dependency graph
+type DependencyGraph map[string][]string
+
+// Function to construct dependency graph from dependency map
+func ConstructDependencyGraph(dependencies map[string][]string) DependencyGraph {
+	graph := make(DependencyGraph)
+	for dependency, versions := range dependencies {
+		graph[dependency] = versions
+	}
+	return graph
+}
+
+// Traverse dependency graph using depth-first search (DFS)
+func DFS(graph DependencyGraph, dependency string, visited map[string]bool, result []string) []string {
+	// Mark current dependency as visited
+	visited[dependency] = true
+
+	// Add current dependency to result
+	result = append(result, dependency)
+
+	// Recursively traverse dependencies
+	for _, dep := range graph[dependency] {
+		if !visited[dep] {
+			result = DFS(graph, dep, visited, result)
+		}
+	}
+
+	return result
+}
+
+// Output dependencies in the same format as go mod graph
+func OutputDependencies(graph DependencyGraph) []string {
+	result := make([]string, 0)
+	visited := make(map[string]bool)
+
+	// Traverse each dependency using DFS
+	for dependency := range graph {
+		if !visited[dependency] {
+			result = DFS(graph, dependency, visited, result)
+		}
+	}
+
+	return result
+}
