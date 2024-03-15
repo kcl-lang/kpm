880da0c46f6fdb5ac6ca45066151fb604be96157
zongz <zongzhe1024@163.com>
2024-03-15 14:46:00 +0800

    feat: Added some methods for sanitizing path on different platforms
    Signed-off-by: zongz <zongzhe1024@163.com>

---
+++ pkg/path/path_test.go
@@ -0,0 +1,46 @@
+package path
+
+import (
+	"runtime"
+	"testing"
+)
+
+func TestSanitizePath(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "Path with null character",
+			input:    "test\x00file",
+			expected: "test_file",
+		},
+		{
+			name:     "Path without invalid characters",
+			input:    "/usr/local/bin/test",
+			expected: "/usr/local/bin/test",
+		},
+	}
+
+	if runtime.GOOS == "windows" {
+		tests = append(tests, struct {
+			name     string
+			input    string
+			expected string
+		}{
+			name:     "Windows style path",
+			input:    "C:\\Program Files\\Test<:>*|",
+			expected: "C:\\Program Files\\Test______",
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			output := SanitizePath(tt.input)
+			if output != tt.expected {
+				t.Errorf("expected %s, got %s", tt.expected, output)
+			}
+		})
+	}
+}
