09d821927534e3e7d48c0c5366427d30c433ef6d
Akash Kumar <meakash7902@gmail.com>
2024-05-17 20:50:32 +0530

    fix ci failure
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

--- pkg/3rdparty/mvs/mvs.go
+++ pkg/3rdparty/mvs/mvs.go
@@ -114,7 +114,7 @@ func buildList(targets []module.Version, reqs Reqs, upgrade func(module.Version)
 	for _, target := range targets {
 		work.Add(target)
 	}
-	work.Do(10, func(m module.Version) {
+	work.Do(1, func(m module.Version) {
 
 		var required []module.Version
 		var err error
