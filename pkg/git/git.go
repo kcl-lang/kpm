4463012e6ab9141e22c1721b6272a1fe0b9955aa
utnim2 <mintugogoi567@gmail.com>
2024-07-26 21:04:02 +0530

    added the feature of checkout from bare repo
    Signed-off-by: utnim2 <mintugogoi567@gmail.com>
    Signed-off-by: Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -126,6 +126,38 @@ func (cloneOpts *CloneOptions) CloneBare() (*git.Repository, error) {
 	return repo, nil
 }
 
+// CheckoutFromBare checks out the specified reference from a bare repository
+func (cloneOpts *CloneOptions) CheckoutFromBare() error {
+    if !cloneOpts.Bare {
+        return errors.New("repository is not bare")
+    }
+
+    repo, err := git.PlainOpen(cloneOpts.LocalPath)
+    if err != nil {
+        return err
+    }
+
+    worktree, err := repo.Worktree()
+    if err != nil {
+        return err
+    }
+
+    checkoutOpts := &git.CheckoutOptions{
+        Force: true,
+    }
+
+    if cloneOpts.Branch != "" {
+        checkoutOpts.Branch = plumbing.NewBranchReferenceName(cloneOpts.Branch)
+    } else if cloneOpts.Tag != "" {
+        checkoutOpts.Branch = plumbing.NewTagReferenceName(cloneOpts.Tag)
+    } else if cloneOpts.Commit != "" {
+        hash := plumbing.NewHash(cloneOpts.Commit)
+        checkoutOpts.Hash = hash
+    }
+
+    return worktree.Checkout(checkoutOpts)
+}
+
 // Clone clones a git repository
 func (cloneOpts *CloneOptions) Clone() (*git.Repository, error) {
 	if err := cloneOpts.Validate(); err != nil {
@@ -169,11 +201,27 @@ func CloneWithOpts(opts ...CloneOption) (*git.Repository, error) {
 	if err != nil {
 		return nil, err
 	}
-	if cloneOpts.Bare {
-		return cloneOpts.CloneBare()
-	}
 
-	return cloneOpts.Clone()
+	var repo *git.Repository
+
+    if cloneOpts.Bare {
+        repo, err = cloneOpts.CloneBare()
+        if err != nil {
+            return nil, err
+        }
+
+        err = cloneOpts.CheckoutFromBare()
+        if err != nil {
+            return nil, err
+        }
+    } else {
+        repo, err = cloneOpts.Clone()
+        if err != nil {
+            return nil, err
+        }
+    }
+
+	return repo, nil
 }
 
 // Clone will clone from `repoURL` to `localPath` via git by tag name.
